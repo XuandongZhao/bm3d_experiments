c170592dbc8b301dbaab2e0d808aa9dffc219f50
Ryan <1018718155@qq.com>
2019-11-13 13:33:39 -0800

    bm3d_dct_bior_1st_2nd_exchange waiting for result

--- src/GEAR/res_name_dencode.py
+++ src/GEAR/res_name_dencode.py
@@ -1,3 +1,5 @@
+import os
+
 def dict2name(d: dict):
     res_name = ''
     for k, v in d.items():
@@ -13,6 +15,32 @@ def name_hyper_res_2_str(name: str, hyper: dict, res: dict):
 def name_hyper_2_str(name: str, hyper: dict):
     name_hyper = name + '+' + dict2name(hyper)
     return name_hyper
+
+
+def find_filename_in_dir(search_dir, include_str_list, exclude_str_list=None):
+    assert os.path.isabs(search_dir)
+    if exclude_str_list is None:
+        exclude_str_list = list()
+    for file_name in os.listdir(search_dir):
+        if not is_include_strs(file_name, *include_str_list):
+            continue
+        if not is_exclude_str(file_name, *exclude_str_list):
+            continue
+        return file_name
+
+
+def is_include_strs(whole_str, *strs):
+    for st in strs:
+        if st not in whole_str:
+            return False
+    return True
+
+
+def is_exclude_str(whole_str, *strs):
+    for st in strs:
+        if st in whole_str:
+            return False
+    return True
 
 
 def is_contain_all(whole_str, name=None, hyper: dict = None, hyper2: dict = None):
---
+++ src/plot_list_dict.py
@@ -0,0 +1,39 @@
+from matplotlib import pyplot as plt
+
+
+def plot_dict_res(*line_dicts, im_label='im_label', x_lable='x_lable', y_label='y_label'):
+    for diction in line_dicts:
+        isinstance(diction, dict)
+        label = diction['name']
+        x_list = diction['x']
+        y_list = diction['y']
+        color = diction.get('color')
+        linestyle = diction.get('linestyle')
+
+        if linestyle is None:
+            linestyle = '--'
+        if color is None:
+            color = 'black'
+
+        plt.plot(x_list, y_list, color=color, linestyle=linestyle, label=label)
+        for x__, y__ in zip(x_list, y_list):
+            plt.text(x__, y__, '%.4f' % y__, ha='center', va='bottom', fontsize=10, color=color)
+
+    plt.rcParams['savefig.dpi'] = 300  # 图片像素
+    plt.rcParams['figure.dpi'] = 300  # 分辨率
+    plt.title(im_label)
+    plt.xlabel(x_lable)
+    plt.ylabel(y_label)
+    plt.legend()
+    return plt
+
+
+if __name__ == '__main__':
+    k_ = ['name', 'x', 'y', 'color', 'linestyle']
+    x_list = [1, 2, 3, 4, 5]
+    y_list = [7, 2, 0, 4, 5]
+    v_ = ['test', x_list, y_list, 'gold', '--']
+    line_dict = dict(zip(k_, v_))
+
+    pltt = plot_dict_res(line_dict)
+    pltt.show()
