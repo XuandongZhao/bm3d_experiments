cb610a00e176463c407d050e25bb6b74053b81dd
Ryan <1018718155@qq.com>
2019-11-18 12:09:07 -0800

    update plot function

--- src/plot_list_dict.py
+++ src/plot_list_dict.py
@@ -2,7 +2,7 @@ from matplotlib import pyplot as plt
 
 
 def plot_dict_res(*line_dicts, im_label='im_label', x_lable='x_lable', y_label='y_label'):
-    for diction in line_dicts:
+    for dict_i, diction in enumerate(line_dicts):
         isinstance(diction, dict)
         label = diction['name']
         x_list = diction['x']
@@ -17,7 +17,15 @@ def plot_dict_res(*line_dicts, im_label='im_label', x_lable='x_lable', y_label='
 
         plt.plot(x_list, y_list, color=color, linestyle=linestyle, label=label)
         for x__, y__ in zip(x_list, y_list):
-            plt.text(x__, y__, '%.4f' % y__, ha='center', va='bottom', fontsize=10, color=color)
+            plt.annotate('%.4f' % y__,  # this is the text
+                         (x__, y__),  # this is the point to label
+                         textcoords="offset points",  # how to position the text
+                         xytext=(8*dict_i, 7*dict_i),  # distance from text to points (x,y)
+                         ha='center',# horizontal alignment can be left, right or center
+                         color=color,
+                         fontsize=7)
+
+                # plt.text(x__, y__, '%.4f' % y__, ha='center', va='bottom', fontsize=10, color=color)
 
     plt.rcParams['savefig.dpi'] = 300  # 图片像素
     plt.rcParams['figure.dpi'] = 300  # 分辨率
