b34881ae3da9f13986fd33eaae6b828cb087c3f8
XuandongZhao <3150104910@zju.edu.cn>
2019-11-18 18:31:41 -0800

    new change

--- src/utils.py
+++ src/utils.py
@@ -54,7 +54,7 @@ def sd_weighting(group_3D):
     std = np.sum(group_3D * group_3D)
 
     res = (std - mean * mean / N) / (N - 1)
-    weight = 1.0 / np.sqrt(res) if res > 0. else 0.
+    weight = 1.0 / np.sqrt(res) if res > 0. else 0
     return weight
 
 
